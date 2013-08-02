b96bb91a1a4a24c1ff40f749321b9975c28b56ed
Steve Anderson <steve@fuzzwork.co.uk>
2013-08-02 15:36:46 +0200

    t2 invention added

--- enterlist.php
+++ enterlist.php
@@ -10,10 +10,12 @@
 <body>
 <p>Paste in a copy and paste from your blueprint list on the S&I interface, and get a page of links to the blueprint details. No details of your blueprints are stored on the server, so you'll have to do this each time you want to see them. The list will be condensed to a count</p>
 <p>To get the list in a useful fashion, open the S&amp;I interface and go to the blueprints tab. open the relevant grey heading. Then double click it. you'll get a window you can C&amp;P from </p>
+<p>The second submit button takes all your blueprints, and gives you a list of everything you can invent from them, without the use of decryptors.</p>
 <form method=post action='bplist.php'>
 <textarea id="entries" name='entries' rows=20 cols=60/>
 </textarea><br />
-<input type=submit value="Enter List" />
+<input type=submit value="Enter List as is" onclick="this.form.action='bplist.php'"/>
+<input type=submit value="Enter List - T2 Invention" onclick="this.form.action='t2list.php'"/>
 </form>
 <?php include('/home/web/fuzzwork/analytics.php'); ?>
 </body>
