a15de44d56bf86344dc1edf6c60791436bc8d9d7
Brian Saydak <brian.saydak@brianum.de>
2014-04-23 18:33:02 +0200

    Some more cleanup

--- format.js
+++ format.js
@@ -4,11 +4,11 @@ function addCommas(nStr) {
 	var x1 = x[0];
 	var x2 = x.length > 1 ? '.' + x[1] : '';
 	var rgx = /(\d+)(\d{3})/;
-	
-	while (rgx.test(x1)) {
+
+	while(rgx.test(x1)) {
 		x1 = x1.replace(rgx, '$1' + ',' + '$2');
 	}
-	
+
 	return x1 + x2;
 }
 
@@ -18,56 +18,65 @@ function addIskCommas(nStr) {
 	var x1 = x[0];
 	var x2 = x.length > 1 ? '.' + x[1] : '.00';
 	var rgx = /(\d+)(\d{3})/;
-	
-	while (rgx.test(x1)) {
+
+	while(rgx.test(x1)) {
 		x1 = x1.replace(rgx, '$1' + ',' + '$2');
 	}
-        if (x2.length<3)
-        {
-            x2=x2+'0';
-        }
+	if(x2.length<3) {
+		x2=x2+'0';
+	}
 	return x1 + x2;
 }
 
 function rectime(secs) {
 	var days = Math.floor(secs / 86400);
-        if (days>0)
-        {
-             secs=secs-(days*86400);
-        }
+	if(days>0) {
+		 secs=secs-(days*86400);
+	}
 	var hr = Math.floor(secs / 3600);
 	var min = Math.floor((secs - (hr * 3600))/60);
 	var sec = secs - (hr * 3600) - (min * 60);
-	
-	if (hr < 10) {hr = "0" + hr; }
-	if (min < 10) {min = "0" + min;}
-	if (sec < 10) {sec = "0" + sec;}
-	if (!hr) {hr = "00";}
-        if (days>1){ hr= days+" days "+hr; } else {if (days>0){hr= days+" day "+hr;}}
+
+	if(hr < 1) hr = '00';
+	else if(hr < 10) hr = '0' + hr;
+	if(min < 10) min = '0' + min;
+	if(sec < 10) sec = '0' + sec;
+
+	if(days>1) {
+		hr= days+' days '+hr;
+	} else if(days>0){
+		hr= days+' day '+hr;
+	}
+
 	return hr + ':' + min + ':' + sec;
 }
 
-function createCookie(name,value,days) {
-	if (days) {
+function createCookie(name, value, days) {
+	if(days) {
 		var date = new Date();
 		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
+		var expires = '; expires='+date.toGMTString();
+	} else {
+		var expires = '';
 	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
+	document.cookie = name+'='+value+expires+'; path=/';
 }
 
 function readCookie(name) {
-	var nameEQ = name + "=";
+	var nameEQ = name + '=';
 	var ca = document.cookie.split(';');
 	for(var i=0;i < ca.length;i++) {
 		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		while(c.charAt(0)==' ') {
+			c = c.substring(1,c.length);
+		}
+		if(c.indexOf(nameEQ) == 0) {
+			return c.substring(nameEQ.length,c.length);
+		}
 	}
 	return null;
 }
 
 function eraseCookie(name) {
-	createCookie(name,"",-1);
+	createCookie(name,'',-1);
 }
--- savebp.php
+++ savebp.php
@@ -1,15 +1,15 @@
 <?php
-if(is_numeric($_GET['typeid']) && is_numeric($_GET['me']) && is_numeric($_GET['pe'])) {
+if(is_numeric($_REQUEST['typeid']) && is_numeric($_REQUEST['me']) && is_numeric($_REQUEST['pe'])) {
 	$currentstructure=json_decode($_COOKIE["blueprints"],true);
-	$newstructure[$_GET['typeid']]['me']=$_GET['me'];
-	$newstructure[$_GET['typeid']]['pe']=$_GET['pe'];
+	$newstructure[$_REQUEST['typeid']]['me']=$_REQUEST['me'];
+	$newstructure[$_REQUEST['typeid']]['pe']=$_REQUEST['pe'];
 	if(is_array($currentstructure)) {
 		$blueprints=array_replace_recursive($currentstructure,$newstructure);
 	} else {
 		$blueprints=$newstructure;
 	}
 	setcookie('blueprints',json_encode($blueprints),time()+31536000);
-	print "Blueprint Saved";
+	echo 'Blueprint Saved';
 } else {
-	print "error occured";
+	echo 'error occured';
 }
